987ca31c79346838b1042946d1069475f9a08fed
sihyeon22 <128593793+sihyeon22@users.noreply.github.com>
2026-05-12 16:48:29 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 0.40 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    for (auto query : queries) {
+        int s = query[0];
+        int e = query[1];
+        int k = query[2];
+        
+        for (int i = s; i <= e; i++) {
+            if (i % k == 0)
+                arr[i]++;
+        }
+    }
+    
+    return arr;
+}
