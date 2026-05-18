d18593de15d074a1181a2ae9f416fce07d60ea44
sihyeon22 <128593793+sihyeon22@users.noreply.github.com>
2026-05-18 14:09:03 +0900

    Update 글자 이어 붙여 문자열 만들기.cpp

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.cpp
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 string solution(string my_string, vector<int> index_list) {
     string answer = "";
+
+    // string 크기 미리 확보 후 재할당 방지 -> push_back() 메모리 누수 방지
+    answer.reserve(index_list.size());
     
     for (int idx : index_list) {
         answer.push_back(my_string[idx]);
