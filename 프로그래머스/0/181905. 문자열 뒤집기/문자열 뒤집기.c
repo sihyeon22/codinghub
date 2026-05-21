4242d17d1d8b0eb21808680140e50b9d18d59673
sihyeon22 <128593793+sihyeon22@users.noreply.github.com>
2026-05-21 21:22:38 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 4.24 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* my_string, int s, int e) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(strlen(my_string) + 1);
+    int i, j;
+    
+    strcpy(answer, my_string);
+    
+    for (i = 0, j = e - s; i < (e - s + 1) / 2; i++, j--) {
+        char tmp = answer[s + i];
+        answer[s + i] = answer[s + j];
+        answer[s + j] = tmp;
+    }
+    
+    return answer;
+}
