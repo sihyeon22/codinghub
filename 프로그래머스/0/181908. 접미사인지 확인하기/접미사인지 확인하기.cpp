0424c94b40d209a3f2cc6cf45a12d2e4ba1b42c3
sihyeon22 <128593793+sihyeon22@users.noreply.github.com>
2026-05-21 16:12:14 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.cpp
@@ -4,16 +4,10 @@
 using namespace std;
 
 int solution(string my_string, string is_suffix) {
-    int size = my_string.length();
-    vector<string> suffix_list(size);
-    
-    for (int i = 0; i < size; i++) {
-        suffix_list.push_back(my_string.substr(i));
-    }
-    
-    for (auto suffix : suffix_list) {
-        if (suffix == is_suffix)
-            return 1;
+    for (int i = 0; i < my_string.length(); i++) {
+        string suffix = my_string.substr(i);
+        
+        if (suffix == is_suffix) return 1;
     }
     
     return 0;
