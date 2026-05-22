d890867eaab86bbe755b9b6112a8a212c9896e7d
sihyeon22 <128593793+sihyeon22@users.noreply.github.com>
2026-05-22 13:25:13 +0900

    [level 0] Title: 글자 지우기, Time: 0.02 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/README.md
+++ 프로그래머스/0/181900. 글자 지우기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 글자 지우기 - 181900 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181900?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181900) 
 
 ### 성능 요약
 
-메모리: 3.86 MB, 시간: 0.01 ms
+메모리: 4.11 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 22일 13:02:45
+2026년 05월 22일 13:25:10
 
 ### 문제 설명
 
--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.cpp
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.cpp
@@ -1,22 +1,15 @@
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 string solution(string my_string, vector<int> indices) {
     string answer = "";
+    set<int> idx_set(indices.begin(), indices.end());
     
     for (int i = 0; i < my_string.length(); i++) {
-        int is_in = 0;
-        
-        for (int idx : indices) {
-            if (i == idx) {
-                is_in = 1;
-                break;
-            }
-        }
-        
-        if (is_in == 0)
+        if (idx_set.count(i) == 0)
             answer.push_back(my_string[i]);
     }
     
