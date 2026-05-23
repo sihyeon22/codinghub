248bf41f3c57e0bcc9073756b90f1eac97f3f587
sihyeon22 <128593793+sihyeon22@users.noreply.github.com>
2026-05-23 16:03:46 +0900

    [level 0] Title: 배열 만들기 3, Time: 13.10 ms, Memory: 20.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+// intervals_rows는 2차원 배열 intervals의 행 길이, intervals_cols는 2차원 배열 intervals의 열 길이입니다.
+int* solution(int arr[], size_t arr_len, int** intervals, size_t intervals_rows, size_t intervals_cols) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int idx = 0;
+    int size1 = intervals[0][1] - intervals[0][0] + 1;
+    int size2 = intervals[1][1] - intervals[1][0] + 1;
+    int* answer = (int*)malloc(sizeof(int) * (size1 + size2));
+    
+    for (int i = 0; i < intervals_rows; i++) {
+        int s = intervals[i][0];
+        int e = intervals[i][1];
+        
+        for (int j = s; j <= e; j++) {
+            answer[idx++] = arr[j];
+        }
+    }
+    
+    return answer;
+}
