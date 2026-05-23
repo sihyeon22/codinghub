2b80ec20fefc56102a3fa15bc145f3cda9d45652
sihyeon22 <128593793+sihyeon22@users.noreply.github.com>
2026-05-23 15:51:12 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = -1;
+    int idx;
+    
+    for (idx = 0; idx < num_list.size(); idx++) {
+        if (num_list[idx] < 0) {
+            answer = idx;
+            break;
+        }
+    }
+    
+    return answer;
+}
